f5985b72f257bd1b00b2dd916c5bfe547c8f5a00
ahyagoub40 <ahyagoub40@gmail.com>
2020-01-08 21:18:44 +0000

    loop through the sentence, skip spaces, add the letters to the count object as keys, their occourances as values

--- countLetters.js
+++ countLetters.js
@@ -4,12 +4,24 @@ const assertEqual = function(actual, expected) {
 
 const countLetters = function(sentence) { // function that accepts a string in a sentence
   const count = {};  // init a object to count the letters later
+  for (let letter of sentence) { // loop through every letter in the sentence
+    if (!(letter === " ")) {  // skip the space
+      // fill the count object. the keys will be the letters, the values will be the number of occurances
+      if (count[letter]) {
+        count[letter] += 1;
+      } else {
+        count[letter] = 1;
+      }
+    }
+
+  }
+  
   return count; // returns the count of each letter
 };
 
-const result = countLetters('LHL'); // call the function and assign the value to a string
+const result = countLetters('lighthouse in the house'); // call the function and assign the value to a string
 
 // check if the actual result is what we expect
-assertEqual(result["H"], 1);
-assertEqual(result["H"], undefined);
-assertEqual(result["L"], 2);
+assertEqual(result["h"], 1);
+assertEqual(result["h"], undefined);
+assertEqual(result["u"], 2);
