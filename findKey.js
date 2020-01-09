54fd51bf403fce15cd7b6903504f500c2e66a1c0
ahyagoub40 <ahyagoub40@gmail.com>
2020-01-09 23:11:46 +0000

    changed callback(key) to callback(object.key) since it is a key in the object

--- findKey.js
+++ findKey.js
@@ -3,8 +3,12 @@ const assertEqual = function(actual, expected) {
 };
 
 const findKey = function(object, callback) { // function that takes an object and a callback function
-  for (const key in object) { // check every key in the object
-    return callback(key);
+
+  for (const key in object) { // check every key (inner object) in the main object
+    const isTrue = callback(object[key]); // to access each property, object.key
+    if (isTrue) { // if value is truthy
+      return key;
+    }
   }
 };
 
@@ -19,4 +23,7 @@ const result = findKey({
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 2);
 
-console.log(result);
+
+assertEqual(result, "noma");
+assertEqual(result, "Ora");
+assertEqual(result, 3);
