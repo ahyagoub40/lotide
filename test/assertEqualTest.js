8b0abef9adab5e8caad048d7bdb8510c8345bb8f
ahyagoub40 <ahyagoub40@gmail.com>
2020-01-13 23:13:28 +0000

    test assert equal function

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual.js');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("mac", "mac");
+assertEqual("Mac", "mac");
+assertEqual(1, 4);
