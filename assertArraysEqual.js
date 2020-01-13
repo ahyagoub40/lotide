392afb83f395e6e2bd4ec26718d7ad04f7d31dd0
ahyagoub40 <ahyagoub40@gmail.com>
2020-01-13 23:51:47 +0000

    removed manual testing and export the function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,36 +1,14 @@
-const assertEqual = function(actual, expected) {
-  (actual === expected) ? console.log(`✅Assertion Passed: ${actual}  === ${expected}`) : console.log(`⛔️Assertion Failed: ${actual} != ${expected}`);
-};
+const eqArrays = require('./eqArrays');
 
-const comapreArrays = function(array1, array2) {
-
-  if (array1.length === array2.length) {
-
-    for (let i = 0; i < array1.length; i++) {
-
-      if (array2[i] !== array1[i]) {
-        return false;
-      }
-    }
-    return true;
-  }  else {
-    return false;
-  }
-};
-assertEqual(comapreArrays([1,2,3], [1,2,3]), true);
-assertEqual(comapreArrays([1,2,3], [3,2,1]), true);
-assertEqual(comapreArrays(["1","2","3"], ["1","2","3"]), true);
-assertEqual(comapreArrays(["1","2","3"], ["1","2",3]), true);
 
 const assertArraysEqual = function(firstArray, secondArray) {
-  if (comapreArrays(firstArray, secondArray)) {
+  if (eqArrays(firstArray, secondArray)) {
     console.log(`✅Assertion Passed: ${firstArray}  === ${secondArray}`);
   } else {
     console.log(`⛔️Assertion Failed: ${firstArray} != ${secondArray}`);
   }
 
 };
-assertArraysEqual([1,2,3], [1,2,3]);
-assertArraysEqual([1,2,3], [3,2,1]);
-assertArraysEqual(["1","2","3"], ["1","2","3"]);
-assertArraysEqual(["1","2","3"], ["1","2",3]);
+
+
+module.exports = assertArraysEqual;
