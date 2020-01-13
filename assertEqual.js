3545e3178a3ea104d060e02447d300969578380c
ahyagoub40 <ahyagoub40@gmail.com>
2020-01-13 23:14:44 +0000

    removed the manual testing and exported the function

--- assertEqual.js
+++ assertEqual.js
@@ -2,11 +2,7 @@ const assertEqual = function(actual, expected) {
   (actual === expected) ? console.log(`✅Assertion Passed: ${actual}  === ${expected}`) : console.log(`⛔️Assertion Failed: ${actual} != ${expected}`);
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("mac", "mac");
-assertEqual("Mac", "mac");
-assertEqual(1, 4);
 
 
+module.exports = assertEqual;
 
