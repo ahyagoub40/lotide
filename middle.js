85122aac62261ea30f0a3f8dac2e6755119b2153
ahyagoub40 <ahyagoub40@gmail.com>
2020-01-13 20:45:34 +0000

    middle function with out manual console test

--- middle.js
+++ middle.js
@@ -1,35 +1,4 @@
-const assertEqual = function(actual, expected) {
-  (actual === expected) ? console.log(`✅Assertion Passed: ${actual}  === ${expected}`) : console.log(`⛔️Assertion Failed: ${actual} != ${expected}`);
-};
 
-const comapreArrays = function(array1, array2) {
-
-  if (array1.length === array2.length) {
-
-    for (let i = 0; i < array1.length; i++) {
-
-      if (array2[i] !== array1[i]) {
-        return false;
-      }
-    }
-    return true;
-  }  else {
-    return false;
-  }
-};
-assertEqual(comapreArrays([1,2,3], [1,2,3]), true);
-assertEqual(comapreArrays([1,2,3], [3,2,1]), true);
-assertEqual(comapreArrays(["1","2","3"], ["1","2","3"]), true);
-assertEqual(comapreArrays(["1","2","3"], ["1","2",3]), true);
-
-const assertArraysEqual = function(firstArray, secondArray) {
-  if (comapreArrays(firstArray, secondArray)) {
-    console.log(`✅Assertion Passed: ${firstArray}  === ${secondArray}`);
-  } else {
-    console.log(`⛔️Assertion Failed: ${firstArray} != ${secondArray}`);
-  }
-
-};
 
 
 const middle = function(array) {
@@ -50,5 +19,5 @@ const middle = function(array) {
   }
    
 };
-assertArraysEqual(middle([1,2,3]), middle([1,2,3]));
-assertArraysEqual(middle([1,2,3]), middle([3,2,1]));
+
+module.exports = middle;
