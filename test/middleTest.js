b0b5b9c6437257e5a3bc1b645b854104179f2dfa
ahyagoub40 <ahyagoub40@gmail.com>
2020-01-14 00:26:53 +0000

    refractored with mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,15 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
 
-assertArraysEqual(middle([1,2,3]), middle([1,2,3]));
-assertArraysEqual(middle([1,2,3]), middle([3,2,1]));
+
+describe('#middle', () => {
+  it("returns [2,3] for [1,2,3,4]", () =>{
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+  it("returns [5] for [4,5,6,]", () =>{
+    assert.deepEqual(middle([4,5,6]), [5]);
+  });
+
+});
+
